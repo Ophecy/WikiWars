191a529700ce27be88c28bf2c3408dcbf3ba89bc
ophecy <arthur.de.berranger@gmail.com>
2020-01-13 14:10:43 +0100

    add: get page title function

--- functions.py
+++ functions.py
@@ -5,3 +5,6 @@ def getRandomWikiPage():
     html = urlopen('https://fr.wikipedia.org/wiki/Sp%C3%A9cial:Page_au_hasard').read()
     soup = BeautifulSoup.BeautifulSoup(html, 'html.parser')
     return soup
+
+def getPageTitle(soup):
+    return soup.title.get_text().replace(' — Wikipédia', '')
